f59e4f0b9a5cdda25200acfd2001ce60c16d1f87
Siphesihle <ST10257085@imconnect.edu.za>
2024-04-01 20:16:15 +0200

    Created the Ingrident Class with the the getter and setter method for Name, Unit and Quantity.

--- POE_recipes/Program.cs
+++ POE_recipes/Program.cs
@@ -1,5 +1,23 @@
 ﻿namespace POE_recipes
 {
+    using System;
+    using System.Collections;
+    using System.Xml.Linq;
+
+    class Ingridents
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double quantity { get; set; }
+
+        public Ingridents(string name, string unit, double quantity)
+        {
+            this.name = name;
+            this.unit = unit;
+            this.quantity = quantity;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
