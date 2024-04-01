3f0af43667b55051ffe0c491d0579a7a958cca8d
Siphesihle <ST10257085@imconnect.edu.za>
2024-04-01 20:26:41 +0200

    1. Created a List using the class Ingridents as the object 2. Created a for-loop for user to enter ingridents 3. Assigned the ingrident data to the list. 4. Wrote a forloop to print ingridents.

--- POE_recipes/Program.cs
+++ POE_recipes/Program.cs
@@ -22,7 +22,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Ingridents> ingridents = new List<Ingridents>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Please enter the Name");
+                string ingri = Console.ReadLine();
+                Console.WriteLine("Please enter the Unit used");
+                string unit = Console.ReadLine();
+                Console.WriteLine("Please the enter how many {0}(s) of {1} you will be needed", unit, ingri);
+                double quantity = Convert.ToDouble(Console.ReadLine());
+
+                var ing = new Ingridents(ingri, unit, quantity);
+                ingridents.Add(ing);
+            }
+
+            foreach (var i in ingridents)
+            {
+                Console.WriteLine("{0} {1} of {2}", i.quantity, i.unit, i.name);
+            }
         }
     }
 }
