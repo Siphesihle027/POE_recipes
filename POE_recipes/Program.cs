1394508eb2042af2107778af11c1a21a7e7008af
Siphesihle <ST10257085@imconnect.edu.za>
2024-04-01 20:12:03 +0200

    Add project files.

---
+++ POE_recipes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace POE_recipes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
