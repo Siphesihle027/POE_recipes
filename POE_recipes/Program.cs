f2051c68964efe4679e5c034fad4567e9134abec
Siphesihle <ST10257085@imconnect.edu.za>
2024-04-11 19:48:46 +0200

    # Finished display Add the option output Add menu list
    Add more outputs

--- POE_recipes/Program.cs
+++ POE_recipes/Program.cs
@@ -2,7 +2,9 @@
 {
     using System;
     using System.Collections;
+    using System.Net.NetworkInformation;
     using System.Xml.Linq;
+    using static System.Console;
 
     class Ingridents
     {
@@ -22,22 +24,42 @@
 
     }
 
-    class Steps
+    class Steps: Ingridents
     {
         public string[] steps { get; set; }
+
+        public void Step(string steps)
+        {
+            this.steps = steps;
+        }
 
 
     }
 
-    class Displays
+    class Display : Steps
     {
         List<Ingridents> ingridents = new List<Ingridents>();
         public void displayR()
         {
             foreach (var ing in ingridents)
             {
-                Console.WriteLine("{0}{1} of {2}", ing.quantity, ing.unit, ing.name);
+                WriteLine("{0}{1} of {2}", ing.quantity, ing.unit, ing.name);
             }
+
+            for (int i = 0; i < steps.Length; i++)
+            {
+                WriteLine("{0}. {1}",(i+1), steps[i]);
+            }
+        }
+
+        public void options()
+        {
+            WriteLine("Please pick your unit of measurement:\n1. Teaspoon\n2.Tablespoon\n3.Cup\n4.No unit");
+        }
+
+        public void menu()
+        {
+            WriteLine("Menu\n1.Enter new recipe\n2.View recipe");
         }
     }
 
@@ -45,27 +67,45 @@
     {
         static void Main(string[] args)
         {
+            int ing_count = 1, step_count = 1;
+            string ingri, unit, step;
+            double quantity;
+
 
             List<Ingridents> ingridents = new List<Ingridents>();
 
-            for (int i = 0; i < 2; i++)
+            WriteLine("Welcome to the Recipe Application");
+            ReadLine();
+
+            WriteLine("How many ingridients will you need for your recipe?");
+            ing_count = Int32.Parse(ReadLine());
+
+
+            for (int i = 0; i < ing_count; i++)
             {
                 Console.WriteLine("Please enter the Name");
-                string ingri = Console.ReadLine();
+                ingri = ReadLine();
                 Console.WriteLine("Please enter the Unit used");
-                string unit = Console.ReadLine();
+                unit = ReadLine();
                 Console.WriteLine("Please the enter how many {0}(s) of {1} you will be needed", unit, ingri);
-                double quantity = Convert.ToDouble(Console.ReadLine());
+                quantity = Convert.ToDouble(ReadLine());
 
                 var ing = new Ingridents(ingri, unit, quantity);
                 ingridents.Add(ing);
 
             }
 
-           /* foreach (var ing in ingridents)
+
+            Console.WriteLine("How many steps are there in your recipe?");
+            step_count = Int32.Parse(ReadLine());
+
+            for (int i = 0; i < step_count; i++)
             {
-                Console.WriteLine("{0} {1} of {2}", ing.quantity, ing.unit, ing.name);
-            }*/
+                Console.WriteLine("Please enter enter step no.{1}",(step_count + 1));
+                step = ReadLine();
+                
+         
+            }
 
 
         }
